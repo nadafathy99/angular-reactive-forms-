a635db22746c6b8d79a9639279af9ddd9fcdaba3
nadafathy99 <53651378+nadafathy99@users.noreply.github.com>
2022-08-31 01:41:44 +0200

    watching on notification value, Duplicating addresses formGroup
    Watching on formControl using valueChanges property.
    Duplicating formGroup using formArray

--- src/app/customers-reactive/customers-reactive.component.ts
+++ src/app/customers-reactive/customers-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 import { Customer } from './customer';
 
 function emailMatcher(c: AbstractControl) : {[key:string]:boolean}|null {
@@ -29,21 +30,38 @@ function checkRange (min:number, max: number): ValidatorFn{
 export class CustomersReactiveComponent implements OnInit {
 
   customer = new Customer();
-  customerForm = this.fb.group({
-    firstName: ['',[Validators.required,Validators.minLength(3)]],
-    lastName: {value:'', disabled:true},
-    emailGroup : this.fb.group({
-      email: ['',[Validators.required,Validators.email]],
-      confirmEmail: ['', Validators.required],
-    }, {validator: emailMatcher}),
-    sendCatalog: true,
-    phone: '',
-    notification: 'email',
-    rating: [null, checkRange(1,5)]
-  })
+  customerForm!:FormGroup;
+  emailMessage!:string;
+  private validationMessages :any = {
+    required:'Email is required',
+    email: 'Please enter a valid email',
+  }
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: ['',[Validators.required,Validators.minLength(3)]],
+      lastName: {value:'', disabled:true},
+      emailGroup : this.fb.group({
+        email: ['',[Validators.required,Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
+      sendCatalog: true,
+      phone: '',
+      notification: 'email',
+      rating: [null, checkRange(1,5)],
+      addresses: this.fb.array([this.buildAddress()]),
+    })
+
+    // By using a watcher to change the validation, we no longer rely on the HTML to notify us of changes to the input element.
+    const notificationControl= this.customerForm.get('notification');
+    notificationControl?.valueChanges.subscribe((change)=> this.setNotification(change));
+
+    const emailControl = this.customerForm.get('email');
+    emailControl?.valueChanges.pipe(
+      debounceTime(1000),
+    ) .subscribe(()=> this.setMessage(emailControl));
   }
 
   save(): void {
@@ -51,17 +69,13 @@ export class CustomersReactiveComponent implements OnInit {
     console.log('Saved: ' + JSON.stringify(this.customerForm?.value));
   }
 
-  /**
-   * setValue vs. patchValue
-   * setValue : set values to ALL form controls, otherwise, ERROR
-   * patchVlue: set values to specific form controls
-   */
   populate() {
     this.customerForm.patchValue({
       firstName: 'nada',
       lastName: 'ahmed',
     })
   }
+
   setNotification(notify:string){
     const phoneControl = this.customerForm.get('phone')
     if (notify=== 'text'){
@@ -71,5 +85,32 @@ export class CustomersReactiveComponent implements OnInit {
       phoneControl?.clearValidators();
     }
     phoneControl?.updateValueAndValidity();
+  }
+
+  setMessage(c:AbstractControl){
+    this.emailMessage='';
+    if ((c.touched||c.dirty) && c.errors){
+      this.emailMessage= Object.keys(c.errors).map((key)=>this.validationMessages[key]).join('') as string
+    }
+  }
+
+  buildAddress():FormGroup{
+    return this.fb.group({
+      addressType: 'home',
+      street1:['',Validators.required],
+      street2:['',Validators.required],
+      city:[''],
+      state:[''],
+      zip:[''],
+    })
+  }
+
+  get addresses():FormArray{
+    // we use a cast operator here to cast it to the desired type; otherwise, the type is an AbstractControl.
+    return <FormArray> this.customerForm.get('addresses');
+  }
+
+  addAddress(){
+    this.addresses.push(this.buildAddress());
   }
 }
